533eb400b6fbc28be81b44213115d1a0a8381eef
Philippe Kueck <projects@unixadm.org>
2022-02-16 08:18:39 +0100

    optparse -> argparse

--- check_sslscan.py
+++ check_sslscan.py
@@ -4,7 +4,7 @@
 # check_sslscan - check server against Qualys sslscan
 # author, (c): Philippe Kueck <projects at unixadm dot org>
 
-from optparse import OptionParser, OptionGroup
+from argparse import ArgumentParser
 
 import ssl
 from urllib.parse import urlencode, quote
@@ -15,6 +15,8 @@ import sys
 import json
 import signal
 import time
+
+__version__ = "1.2"
 
 scores = {
     'A+': 7, 'A':  6, 'A-': 5, 'B':  4, 'C':  3,
@@ -111,62 +113,63 @@ if __name__ == "__main__":
     signal.signal(signal.SIGALRM, timeouthandler)
     signal.alarm(900)
 
-    desc = "%prog check server's ssl score"
-    parser = OptionParser(description=desc, version="%prog version 1.1")
+    parser = ArgumentParser(
+        description="%(prog)s: check server's ssl score"
+    )
 
-    gen_opts = OptionGroup(parser, "Generic options")
-    thres_opts = OptionGroup(parser, "Threshold options")
-    parser.add_option_group(gen_opts)
-    parser.add_option_group(thres_opts)
+    gen_opts = parser.add_argument_group("Generic options")
+    thres_opts = parser.add_argument_group("Threshold options")
 
-    thres_opts.add_option("-w", "--warn", dest="warn",
-        type="string", action="store", default="B",
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + __version__)
+
+    thres_opts.add_argument("-w", "--warn", dest="warn",
+        type=str, metavar="RATING", action="store", default="B",
+        choices=list(scores),
         help="warn at or below this grade"
     )
 
-    thres_opts.add_option("-c", "--crit", dest="crit",
-        type="string", action="store", default="C",
+    thres_opts.add_argument("-c", "--crit", dest="crit",
+        type=str, metavar="RATING", action="store", default="C",
+        choices=list(scores),
         help="crit at or below this grade"
     )
 
-    gen_opts.add_option("-H", "--host", dest="host",
-        type="string", action="store",
+    gen_opts.add_argument("-H", "--host", dest="host",
+        type=str, action="store", required=True,
         help="hostname to check"
     )
 
-    gen_opts.add_option("-i", "--ip", dest="ip",
-        type="string", action="store",
+    gen_opts.add_argument("-i", "--ip", dest="ip",
+        type=str, action="store",
         help="ip address to use if the host has more than one endpoint"
     )
 
-    gen_opts.add_option("-t", "--ignore-trust", dest="ignoretrust",
+    gen_opts.add_argument("-t", "--ignore-trust", dest="ignoretrust",
         action="store_true", default=False,
         help="retrieve the trust-adjusted score(s), ignoring certificate trust issues"
     )
 
-    gen_opts.add_option("-a", "--maxage", dest="maxage",
-        type="int", metavar="HOURS", action="store", default=0,
+    gen_opts.add_argument("-a", "--maxage", dest="maxage",
+        type=int, metavar="HOURS", action="store", default=0,
         help="set max cache age in hours (unless --nocache is set)"
     )
 
-    gen_opts.add_option("-x", "--nocache", dest="nocache",
+    gen_opts.add_argument("-x", "--nocache", dest="nocache",
         action="store_true", default=False,
         help="do not accept cached results"
     )
 
-    gen_opts.add_option("-p", "--publish", dest="publish",
+    gen_opts.add_argument("-p", "--publish", dest="publish",
         action="store_true", default=False,
         help="publish results at Qualys SSL Labs"
     )
 
-    gen_opts.add_option("-d", "--debug", dest="debug",
+    gen_opts.add_argument("-d", "--debug", dest="debug",
         action="store_true", default=False,
         help="debug mode"
     )
-    (options, args) = parser.parse_args()
 
-    if options.warn not in scores or options.crit not in scores:
-        nagexit(3, "invalid score specified")
+    options = parser.parse_args()
 
     main()
 
